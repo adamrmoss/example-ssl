4f75634beb72f76afa28837a188d88bdf94076c0
Adam R Moss <adamrmoss@gmail.com>
2023-01-23 18:56:05 -0500

    More config changes

--- request-cert.sh
+++ request-cert.sh
@@ -1,2 +1,2 @@
 # This script is to be run on AWS
-sudo certbot certonly --webroot -w ~/example-ssl -d hb-example-ssl.com
+sudo ./env/bin/certbot certonly --webroot -w ~/example-ssl -d hb-example-ssl.com
